a37489f1dbe43c57c5e12bd612344386615e00ee
seongmin221 <seongmin221@naver.com>
2022-04-19 11:02:21 +0900

    initial commit

---
+++ Swift_SampleAppsTutorial/Swift_SampleAppsTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift_SampleAppsTutorial
+//
+//  Created by 이성민 on 2022/04/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift_SampleAppsTutorial/Swift_SampleAppsTutorial/Swift_SampleAppsTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_SampleAppsTutorialApp.swift
+//  Swift_SampleAppsTutorial
+//
+//  Created by 이성민 on 2022/04/19.
+//
+
+import SwiftUI
+
+@main
+struct Swift_SampleAppsTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
